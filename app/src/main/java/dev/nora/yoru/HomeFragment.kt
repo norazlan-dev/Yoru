5c97c98432d33727d4909df13ffacd151e5016a8
norazlan <mohdnorazlan.yasin@gmail.com>
2023-04-12 16:34:09 +0800

    update: fragment

--- app/src/main/java/dev/nora/yoru/HomeFragment.kt
+++ app/src/main/java/dev/nora/yoru/HomeFragment.kt
@@ -8,21 +8,15 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import dev.nora.yoru.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
