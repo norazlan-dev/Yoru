6f97a8e60dffb814f42e9d60d7e8bd6e7450c485
norazlan <mohdnorazlan.yasin@gmail.com>
2023-04-14 12:52:08 +0800

    update: chat

--- app/src/main/java/dev/nora/yoru/ChatFragment.kt
+++ app/src/main/java/dev/nora/yoru/ChatFragment.kt
@@ -1,14 +1,13 @@
 package dev.nora.yoru
 
 import android.os.Bundle
-import android.view.Gravity
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.navigation.fragment.findNavController
+import androidx.cardview.widget.CardView
 import dev.nora.yoru.databinding.FragmentChatBinding
+import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
@@ -30,39 +29,102 @@ class ChatFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val words = listOf(
-            "APPLE", "BANANA", "CHERRY", "DATE", "ELDERBERRY", "FIG",
-            "GRAPE", "HONEYDEW", "KIWI", "LEMON", "MANGO", "NECTARINE",
-            "ORANGE", "PEACH", "QUINCE", "RASPBERRY", "STRAWBERRY", "TANGERINE",
-            "UGLI FRUIT", "WATERMELON"
+            "Hello, how are you? How's it going?",
+            "What's up?",
+            "Nice to meet you. How's it going?",
+            "How's your day?",
+            "Long time no see",
+            "Good morning! How's it going?",
+            "Good afternoon!",
+            "Good evening!",
+            "How's it going?",
+            "Hello, how are you? What have you been up to?"
         )
-        var x = 0
+        var from = ""
 
         binding.btnSend.setOnClickListener {
+            val date = LocalDate.now()
+            val dayOfWeekFormatter = DateTimeFormatter.ofPattern("EEEE")
+            val formattedDayOfWeek = date.format(dayOfWeekFormatter)
+            val dayOfMonth = date.dayOfMonth
+            val monthFormatter = DateTimeFormatter.ofPattern("MMM")
+            val formattedMonth = date.format(monthFormatter)
             val time = LocalTime.now()
-            val formatter = DateTimeFormatter.ofPattern("h:mm:ss a")
-            val formattedTime = time.format(formatter)
+            val timeFormatter = DateTimeFormatter.ofPattern("h:mm a")
+            val formattedTime = time.format(timeFormatter)
+            val formattedDateTime = "$formattedDayOfWeek, $dayOfMonth $formattedMonth : $formattedTime"
 
-            if (x % 2 == 0) {
-                val messageView = TextView(context)
-                messageView.text = "Hello, ${words.random()}!"
-                messageView.gravity = Gravity.END
-                val messageViewTime = TextView(context)
-                messageViewTime.text = "Sent $formattedTime"
-                messageViewTime.gravity = Gravity.END
-                binding.chatFrame.addView(messageView)
-                binding.chatFrame.addView(messageViewTime)
-                binding.chatFrameScroll.fullScroll(View.FOCUS_DOWN)
+            if (binding.chatFrame.childCount > 0 && from == "send") {
+                val lastMessageView = binding.chatFrame.getChildAt(binding.chatFrame.childCount - 1) as LinearLayout
+                val lastMessageTextView = lastMessageView.findViewById<TextView>(R.id.message_time)
+                if (lastMessageTextView.text.toString() == formattedDateTime) {
+                    lastMessageTextView.visibility = View.GONE
+                }
             }
-            else {
-                val messageView = TextView(context)
-                messageView.text = "Hello,  ${words.random()}!"
-                val messageViewTime = TextView(context)
-                messageViewTime.text = "Sent $formattedTime"
-                binding.chatFrame.addView(messageView)
-                binding.chatFrame.addView(messageViewTime)
-                binding.chatFrameScroll.fullScroll(View.FOCUS_DOWN)
+
+            val messageView = LayoutInflater.from(context).inflate(R.layout.asset_chatbox, null) as LinearLayout
+            messageView.findViewById<TextView>(R.id.message_text).text = "Hello, ${words.random()} ${words.random()}"
+            messageView.findViewById<TextView>(R.id.message_time).text = "$formattedDateTime"
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.gravity = Gravity.END
+            layoutParams.topMargin = context?.resources?.getDimensionPixelSize(R.dimen.margin_8dp)!!
+            layoutParams.marginStart = context?.resources?.getDimensionPixelSize(R.dimen.margin_32dp)!!
+            messageView.layoutParams = layoutParams
+            binding.chatFrame.addView(messageView)
+            from = "send"
+            binding.chatFrameScroll.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    binding.chatFrameScroll.post {
+                        binding.chatFrameScroll.fullScroll(View.FOCUS_DOWN)
+                    }
+                    binding.chatFrameScroll.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
+        }
+
+        binding.btnReceive.setOnClickListener {
+            val date = LocalDate.now()
+            val dayOfWeekFormatter = DateTimeFormatter.ofPattern("EEEE")
+            val formattedDayOfWeek = date.format(dayOfWeekFormatter)
+            val dayOfMonth = date.dayOfMonth
+            val monthFormatter = DateTimeFormatter.ofPattern("MMM")
+            val formattedMonth = date.format(monthFormatter)
+            val time = LocalTime.now()
+            val timeFormatter = DateTimeFormatter.ofPattern("h:mm a")
+            val formattedTime = time.format(timeFormatter)
+            val formattedDateTime = "$formattedDayOfWeek, $dayOfMonth $formattedMonth : $formattedTime"
+
+            if (binding.chatFrame.childCount > 0 && from == "receive") {
+                val lastMessageView = binding.chatFrame.getChildAt(binding.chatFrame.childCount - 1) as LinearLayout
+                val lastMessageTextView = lastMessageView.findViewById<TextView>(R.id.message_time)
+                if (lastMessageTextView.text.toString() == formattedDateTime) {
+                    lastMessageTextView.visibility = View.GONE
+                }
             }
-            x++
+
+            val messageView = LayoutInflater.from(context).inflate(R.layout.asset_chatbox_alt, null) as LinearLayout
+            messageView.findViewById<TextView>(R.id.message_text).text = "Hey, ${words.random()} ${words.random()}"
+            messageView.findViewById<TextView>(R.id.message_time).text = "$formattedDateTime"
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            messageView.layoutParams = layoutParams
+            layoutParams.topMargin = context?.resources?.getDimensionPixelSize(R.dimen.margin_8dp)!!
+            layoutParams.marginEnd = context?.resources?.getDimensionPixelSize(R.dimen.margin_32dp)!!
+            binding.chatFrame.addView(messageView)
+            from = "receive"
+            binding.chatFrameScroll.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    binding.chatFrameScroll.post {
+                        binding.chatFrameScroll.fullScroll(View.FOCUS_DOWN)
+                    }
+                    binding.chatFrameScroll.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
         }
     }
 
